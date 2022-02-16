835ab8f1be7b8ed1da6a1b0080b6f2f00b381216
Théo Boyer <theob@Air-de-Theo.com>
2022-02-16 15:59:20 +0100

    backend to load images metadata + endpoint image_to_annotate

--- index.js
+++ index.js
@@ -1,9 +1,58 @@
 const express = require('express');
+const fs = require("fs");
+const csv = require('csv')
 const app = express();
 
-app.use("/image_to_annotate", (req, res) => {
-      
-});
-app.use(express.static('public'));
+port=3000;
 
-app.listen(3000);
+const annotations = [];
+function readAnnotations(cb) {
+    fs.createReadStream('annotations.csv')
+    .pipe(csv.parse({
+        columns:true
+    }))
+    .on('data', (row) => {
+        images_not_annotated.delete(row.image);
+        annotations.push(row);
+    })
+    .on('end', () => {
+        console.log('annotations.csv successfully processed');
+        console.log(`${metadata.length} images in total`);
+        console.log(`${images_not_annotated.size} images yet to be annotated`);
+        images_not_annotated = Array.from(images_not_annotated);
+        cb();
+    });
+}
+
+const metadata = [];
+let images_not_annotated = new Set();
+function readData(cb) {
+    fs.createReadStream('metadata.csv')
+    .pipe(csv.parse({
+        columns:true
+    }))
+    .on('data', (row) => {
+        images_not_annotated.add(row.image);
+        metadata.push(row);
+    })
+    .on('end', () => {
+        console.log('metadata.csv successfully processed');
+        readAnnotations(cb);
+    });
+}
+
+function setupServer() {
+    app.use("/image_to_annotate", (req, res) => {
+        res.json({
+            "image": images_not_annotated[Math.floor(Math.random() * images_not_annotated.length)]
+        })
+    });
+    app.use(express.static('public'));
+    app.use("/images", express.static('images'));
+    
+    app.listen(port, () => {
+        console.log(`Example app listening on port ${port}`)
+    })
+}
+
+readData(setupServer);
